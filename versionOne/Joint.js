85461831cc65c7e5462898f3e1e0bf3ced4a1389
dourabbit <dourabbit@gmail.com>
2012-02-18 15:33:18 -0500

    asf parser

---
+++ versionOne/Joint.js
@@ -0,0 +1,27 @@
+
+var joint = (function () {
+    // private static
+    //var nextId = 1;
+
+    // constructor
+    var cls = function (parameters) {
+     
+
+        this.Initialize = function () {
+
+        
+        }
+       
+    };
+
+
+//    cls.prototype.GetRay = function (winx, winy, winz, invMatrix, viewMatrix, viewHeight, viewWidth, fov, point, direction) {
+//     
+
+//    }
+
+
+
+
+    return cls;
+})();
